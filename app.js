4cac644f9f940f5f734cd4948ab1dc559adcc0de
AyaanZaidi <103330931+AyaanZaidi@users.noreply.github.com>
2022-11-03 15:18:00 +0500

    This is my practice
    This is my practice

---
+++ app.js
@@ -0,0 +1,58 @@
+//chapter: 9 start.
+var value = prompt("Enter your course name");
+console.log(value)
+// chapter: 9 complete.
+
+// chapter: 10 start.
+var x = prompt("Where does the Pope live?");
+ if (x === "correct") {
+ console.log("Correct!");
+}
+// chapter: 10 complete.
+
+// chapter: 11 start.
+var b = prompt("hussain");
+if(b === "hussain"){
+    console.log("correct")
+
+}
+// chapter: 11 complete.
+
+ // chapter: 12 start.
+var officer = prompt("officer");
+if(officer === "true"){
+    console.log("work is submited")
+}else if( officer==="false"){
+    console.log("work is not submited")
+}else{
+    console.log("wapsi ghar" )
+}
+ // chapter: 12 complete.
+
+ // chapter: 13 start.
+ var weight = prompt("Enter a Weigth")
+ var time = prompt("Enter a time")
+ var age = prompt("Enter a age")
+ var gender = prompt("Enter a gendermale")
+
+ if(weight > 300 || time < 6 || age > 17 || gender === "male"){
+    console.log("Come to our tryout!")
+ }else{
+     console.log("Come to our cookout!")
+ }
+ // chapter: 13 complete.
+
+ // chapter: 14 start.
+ var male = prompt("Enter a gender")
+ var male1 = prompt("Enter a female")
+if(male === "gender"){
+    console.log("gender Name")
+    if(male1 === "female"){
+        console.log("enter a correct gender")
+    }else{
+        console.log("uncorrect gender")
+    }
+}else{
+    console.log("not a male name")
+}
+ // chapter: 14 complete.
